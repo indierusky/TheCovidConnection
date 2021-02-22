87bd24eda27d7c6aff72e75a9b57c8540ea9cad8
indierusky <indierusky@gmail.com>
2021-02-22 00:44:12 -0500

    updated navs and profiles

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -40,49 +40,8 @@ class About extends React.Component {
                             
                         </div>
 
-                     <div className="person-div">
-                            <h2> Divyam Satyarthi </h2>
-                            <div className="links">
-                                <div className="link-text-image">
-                                    <a href="https://github.com/divyams1">
-                                        <FontAwesomeIcon className="about-lnks" icon={faGithub} />
-
-                                        <p> Github </p>
-                                    </a>
-                                </div>
-                                <div className="link-text-image">
-                                    <a href="https://www.linkedin.com/in/divyam-satyarthi-b6628513b/">
-                                     <FontAwesomeIcon className="about-lnks" icon={faLinkedinIn} />
-
-                                    <p> LinkedIn</p>
-                                    </a>
-                                </div>
-                            </div>
-                            <h4> Email: divyamsat@gmail.com </h4>
-                            <h4> Phone: 443-838-7815</h4>
-                        </div>
-
-                     <div className="person-div">
-                            <h2> Anthony Collichio </h2>
-                            <div className="links">
-                                    <div className="link-text-image">
-                                <a  href="https://github.com/collich55">
-                                        <FontAwesomeIcon className="about-lnks" icon={faGithub} />
-
-                                            <p> Github</p>
-                                            </a>
-                                    </div>
-                                    <div className="link-text-image">
-                                <a  href="https://www.linkedin.com/in/anthony-collichio-451b11103/"> 
-                                        <FontAwesomeIcon className="about-lnks" icon={faLinkedinIn} />
-
-                                            <p> LinkedIn </p>
-                                        </a>
-                                    </div>
-                            </div>
-                            <h4> Email: collich55@gmail.com  </h4>
-                            <h4> Phone: 585-794-3850  </h4>
-                        </div>
+                    
+                  
                         
                     </div>
                     <Link id="back-link" to={'/'}>Back</Link>
